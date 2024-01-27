9db97a4ef8de74ad67f4ba153e62d6e54fe9df93
ayadav <123587824+ayadav313@users.noreply.github.com>
2024-01-27 00:20:55 -0500

    init

---
+++ dripBoarding/frontend/src/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import { Navbar, Nav, Container, Carousel } from 'react-bootstrap'; // Import Bootstrap components
+
+import logo from './logo.svg'; // Import your logo
+
+function App() {
+  return (
+    <div>
+      {/* Navigation Bar */}
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+            Your App Name
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* Bootstrap Carousel */}
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://via.placeholder.com/800x400"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://via.placeholder.com/800x400"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        {/* Add more Carousel.Items as needed */}
+      </Carousel>
+
+      {/* Main Content */}
+      <Container>
+        <h1>Your App Content Goes Here</h1>
+        {/* Add your app content here */}
+      </Container>
+    </div>
+  );
+}
+
+export default App;
