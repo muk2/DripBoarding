fc5d7270734d71a19b26b6db70775fd10b15f1bd
Mukund Chanchlani <malchal@mukunds-mbp.mynetworksettings.com>
2024-02-01 22:21:55 -0500

    bruv

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,92 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="ecommerce">
+    <h1>Welcome to our E-Commerce Store</h1>
+    <div class="product-list">
+      <div v-for="(product, index) in products" :key="index" class="product-card">
+        <img :src="product.image" alt="Product Image" class="product-image">
+        <h2>{{ product.name }}</h2>
+        <p>{{ product.description }}</p>
+        <p>Price: ${{ product.price }}</p>
+        <button @click="addToCart(product)">Add to Cart</button>
+      </div>
+    </div>
+    <h2>Your Shopping Cart</h2>
+    <div class="cart">
+      <div v-for="(item, index) in cart" :key="index" class="cart-item">
+        <p>{{ item.name }}</p>
+        <p>Price: ${{ item.price }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: 'EcommercePage',
+  data() {
+    return {
+      products: [
+        {
+          name: 'Poori GAAND',
+          description: 'Description of Product 1',
+          price: 19.99,
+          image: 'product1.jpg',
+        },
+        {
+          name: 'Product 2',
+          description: 'Description of Product 2',
+          price: 29.99,
+          image: 'product2.jpg',
+        },
+        // Add more products here
+      ],
+      cart: [],
+    };
+  },
+  methods: {
+    addToCart(product) {
+      this.cart.push(product);
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h1 {
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.product-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.product-card {
+  border: 1px solid #ccc;
+  padding: 20px;
+  margin: 10px;
+  text-align: center;
+  width: 250px;
 }
-a {
-  color: #42b983;
+.product-image {
+  max-width: 100%;
+  height: auto;
+}
+button {
+  background-color: #42b983;
+  color: #fff;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+.cart {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.cart-item {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 5px;
+  width: 200px;
 }
 </style>
